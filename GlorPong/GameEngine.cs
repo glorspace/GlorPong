fad712a5ff2e7807011b3d37b093646c1bc4820a
simsto <simsto@LEVIATHAN>
2017-02-14 20:23:19 -0500

    Renamed Game1 to GameEngine.

--- GlorPong/GameEngine.cs
+++ GlorPong/GameEngine.cs
@@ -7,12 +7,12 @@ namespace GlorPong
 	/// <summary>
 	/// This is the main type for your game.
 	/// </summary>
-	public class Game1 : Game
+	public class GameEngine : Game
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
-		public Game1()
+		public GameEngine()
 		{
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
